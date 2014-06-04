5d05f9e3d2c88c69eb827b48881141400cad5bcd
JSantosP <javiersantospaniego@gmail.com>
2014-06-04 17:09:04 +0200

    Scala code was propperly formatted

--- src/main/scala/dsl/Lolcode.scala
+++ src/main/scala/dsl/Lolcode.scala
@@ -2,71 +2,71 @@ package dsl
 
 object LolCode extends statements {
 
-	/**
-	  * Ussage:
-	  * 	CAN HAS <library:String> ?
-	  */
-	class Can(l: Library){
-		def ? : Statement = ImportStatement(l)
-	}
-	object CAN {
-		def HAS(library: Library): Can = new Can(library)
-	}
+  /**
+    * Ussage:
+    *   CAN HAS <library:String> ?
+    */
+  class Can(l: Library){
+    def ? : Statement = ImportStatement(l)
+  }
+  object CAN {
+    def HAS(library: Library): Can = new Can(library)
+  }
 
-	/**
-	  * Ussage:
-	  * 	PLZ OPEN FILE <fileName:String> ?
-	  */
-	class Plz(f: File){
-		def ? : Statement = OpenFileStatement(f)
-	}
-	object PLZ {
-		def OPEN(file: File): Plz = new Plz(file)
-	}
-	object FILE{
-		def apply(name: String): File = new File(name)
-	}
+  /**
+    * Ussage:
+    *   PLZ OPEN FILE <fileName:String> ?
+    */
+  class Plz(f: File){
+    def ? : Statement = OpenFileStatement(f)
+  }
+  object PLZ {
+    def OPEN(file: File): Plz = new Plz(file)
+  }
+  object FILE{
+    def apply(name: String): File = new File(name)
+  }
 
-	/**
-	  * Ussage:
-	  * 	VISIBLE <msg: String>
-	  *		VISIBLE FILE
-	  */
-	object VISIBLE{
-		def apply(s: String): Statement =
-			PrintStatement(s,visible=true)
-		def FILE: Statement = 
-			PrintFileStatement(visible=true)
-	}
+  /**
+    * Ussage:
+    *   VISIBLE <msg: String>
+    *    VISIBLE FILE
+    */
+  object VISIBLE{
+    def apply(s: String): Statement =
+      PrintStatement(s,visible=true)
+    def FILE: Statement = 
+      PrintFileStatement(visible=true)
+  }
 
-	/**
-	  * Ussage:
-	  * 	INVISIBLE <error: String>
-	  */
-	object INVISIBLE{
-		def apply(msg: String): Statement = 
-			PrintStatement(msg,visible=false)
-		def FILE: Statement = 
-			PrintFileStatement(visible=false)
-	}
+  /**
+    * Ussage:
+    *   INVISIBLE <error: String>
+    */
+  object INVISIBLE{
+    def apply(msg: String): Statement = 
+      PrintStatement(msg,visible=false)
+    def FILE: Statement = 
+      PrintFileStatement(visible=false)
+  }
 
-	/**
-	  * Ussage:
-	  * 	AWSUM THX { <okCondition: Statement> } O_NOES { <exceptionCondition: Statement> }
-	  */
-	object AWSUM{
-		def THX(okStatement: Statement) = new ConditionalExecution(okStatement)
-	}
+  /**
+    * Ussage:
+    *   AWSUM THX { <okCondition: Statement> } O_NOES { <exceptionCondition: Statement> }
+    */
+  object AWSUM{
+    def THX(okStatement: Statement) = new ConditionalExecution(okStatement)
+  }
 
-	// Helpers
+  // Helpers
 
-	object HAI {
-		def apply(statements: Statement*): Program = 
-			Program(Map(),statements)
-	}
+  object HAI {
+    def apply(statements: Statement*): Program = 
+      Program(Map(),statements)
+  }
 
-	implicit class programHelper(p: Program) {
-		def KTHXBYE: Program = Program(p.env,p.statements,isSyntaxOk=true)
-	}
+  implicit class programHelper(p: Program) {
+    def KTHXBYE: Program = Program(p.env,p.statements,isSyntaxOk=true)
+  }
 
 }
--- src/main/scala/dsl/common.scala
+++ src/main/scala/dsl/common.scala
@@ -5,7 +5,7 @@ package dsl
   * a set of functionality to be imported
   */
 trait Library{
-	val name: String
+  val name: String
 }
 
 /** 
@@ -13,33 +13,33 @@ trait Library{
   * a set of imported libraries.
   */
 case class Program (
-	val env: Map[String, Any],
-	val statements: Seq[Statement],
-	val imports: Set[Library] = Set(), 
-	val isSyntaxOk: Boolean = false,
-	val exceptionThrown: Boolean = false){
+  val env: Map[String, Any],
+  val statements: Seq[Statement],
+  val imports: Set[Library] = Set(), 
+  val isSyntaxOk: Boolean = false,
+  val exceptionThrown: Boolean = false){
 }
 
 /** A Statement acts just like a state transformer when it's executed*/
 trait Statement{
-	def execute(implicit context: Program):Program
+  def execute(implicit context: Program):Program
 }
 
 object common {
 
-	def run(program: Program): Program =
-		if (!program.isSyntaxOk) program
-		else (program /: program.statements)((context, statement) =>
-			statement.execute(context))
+  def run(program: Program): Program =
+    if (!program.isSyntaxOk) program
+    else (program /: program.statements)((context, statement) =>
+      statement.execute(context))
 
-	// Libraries
+  // Libraries
 
-	case object STDIO extends Library { val name = "stdio" }
+  case object STDIO extends Library { val name = "stdio" }
 
-	// Helpers
+  // Helpers
 
-	implicit def emptyStatement(u: Unit): Statement = new Statement{
-		def execute(implicit context: Program):Program = context
-	}
+  implicit def emptyStatement(u: Unit): Statement = new Statement{
+    def execute(implicit context: Program):Program = context
+  }
 
 }
